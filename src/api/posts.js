19e4e115564cdab2cf575f10a2e98e6e80a432cd
vvenommm <55tggcc@gmail.com>
2022-12-27 16:57:50 +0900

    글 등록 validation 메시지 alert까지 구현. 등록도 가능

--- src/api/posts.js
+++ src/api/posts.js
@@ -4,7 +4,7 @@ export function getPosts() {
 	return axios.get('http://localhost:8080/main/1');
 }
 export function getPostsOfAnotherPage(page) {
-	return axios.get(`http://localhost:8080/main/${page}`);
+	return axios.get(`http://localhost:8080/main/${page}`).then;
 }
 
 export function readPost(currentPage, num) {
@@ -12,4 +12,10 @@ export function readPost(currentPage, num) {
 	console.log('cp : ', currentPage);
 	console.log('post num : ', num);
 	return axios.get(`http://localhost:8080/read/${currentPage}/${num}`);
+}
+
+export function writePost(post) {
+	console.log('post : ', post.value);
+	return axios.post('http://localhost:8080/writePost', post.value);
+	// return axios.post('http://localhost:8080/writePost', post);
 }
--- src/views/WritePost.vue
+++ src/views/WritePost.vue
@@ -1,61 +1,89 @@
 <script setup>
 import { ref } from 'vue';
-import data from '@/data';
+// import data from '@/data';
 import { useRouter } from 'vue-router';
-import axios from 'axios';
+// import axios from 'axios';
+import { writePost } from '@/api/posts';
 
 // ref -> number, string
 // reactive -> {}
 
 // const route = useRoute();
-console.log('WritePost - board : ', data.Posts);
+// console.log('WritePost - board : ', data.Posts);
 
 const catList = [
 	{
 		text: '질문',
-		value: '질문',
+		value: 'q',
 	},
 	{
 		text: '자유',
-		value: '자유',
+		value: 'f',
 	},
 	{
 		text: '기타',
-		value: '기타',
+		value: 'e',
 	},
 ];
 
 const post = ref({
-	bnum: 0,
+	// bnum: 0,
 	category: '',
 	title: '',
-	writer: '',
+	nickname: '',
 	contents: '',
-	dates: '22/12/22',
+	// dates: '22/12/22',
 	comments: 0,
 	views: 0,
 });
 
-const addPost = () => {
-	post.value.bnum = data.Posts.length;
-	console.log('작성한 글', post);
-	console.log('before push --', data);
-	console.log('before length --', data.Posts.length);
-	var cnt = data.Posts.push(post.value);
-	console.log('결과 ', cnt);
-	console.log('after push --', data);
-	if (parseInt(cnt) == post.value.bnum + 1) {
-		const router = useRouter();
-		// router.push({ name: 'Read', params: { bnum: post.value.bnum } });
-		router.push({ path: '/' });
+const errors = ref([]);
 
-		// router.replace({ path: '/' });
+const addPost = async () => {
+	// post.value.bnum = data.Posts.length;
+	console.log('작성한 글', post.value);
+	// console.log('before push --', data);
+	// console.log('before length --', data.Posts.length);
+	// var cnt = data.Posts.push(post.value);
+	// console.log('결과 ', cnt);
+	// console.log('after push --', data);
+	// if (parseInt(cnt) == post.value.bnum + 1) {
+	// const router = useRouter();
+	// router.push({ name: 'Read', params: { bnum: post.value.bnum } });
+	// router.push({ path: '/' });
+
+	// router.replace({ path: '/' });
+	// } else {
+	// alert('오류 발생');
+	// }
+
+	const response = await writePost(post);
+	console.log('글 등록 후 결과 : ', response.data);
+	if (response.data.result == 0) {
+		errors.value = response.data.errors;
+		console.log(errors.value);
+		if (errors.value.msg_category != null) {
+			alert(errors.value.msg_category);
+			return;
+		}
+		if (errors.value.msg_title != null) {
+			alert(errors.value.msg_title);
+			return;
+		}
+		if (errors.value.msg_nickname != null) {
+			alert(errors.value.msg_nickname);
+			return;
+		}
+		if (errors.value.msg_contents != null) {
+			alert(errors.value.msg_contents);
+			return;
+		}
 	} else {
-		alert('오류 발생');
+		const router = useRouter();
 	}
 };
 
-console.log('post', post);
+console.log('post', post.value);
 </script>
 
 <template>
@@ -77,7 +105,7 @@ console.log('post', post);
 				v-model="post.title"
 				placeholder="제목을 입력해주세요."
 			/>
-			<input type="text" v-model="post.writer" placeholder="닉네임" />
+			<input type="text" v-model="post.nickname" placeholder="닉네임" />
 		</div>
 		<div class="mb-3">
 			<label for="contents" class="form-label">내용</label>
